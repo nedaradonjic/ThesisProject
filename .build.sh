36abb12537299ff77be238059a15001d56b8b4f5
nedaradonjic <102152013+nedaradonjic@users.noreply.github.com>
2023-12-06 08:24:39 +0100

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+
+# Print current directory for debugging
+echo "Current directory: $(pwd)"
+
 # Navigate to the travelagency directory
 cd /opt/render/project/travelagency
+
+# Print contents of the directory for debugging
+echo "Directory contents: $(ls -la)"
 
 # Install dependencies
 pip install -r requirements.txt
